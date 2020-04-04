34aaf13c1ece627a29e20df1d50a4cdcffd1884e
arman.boyaci <arman.boyaci@algosis.com.tr>
2020-04-04 23:50:19 +0300

    data visualization

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -12,6 +13,7 @@
 #     name: vertexcover
 # ---
 
+import pandas as pd
 import networkx as nx
 from networkx.algorithms.approximation import min_weighted_vertex_cover
 import matplotlib.pyplot as plt
@@ -19,34 +21,87 @@ import matplotlib.pyplot as plt
 with open('data/neighbor_cities_in_Turkey.txt', 'r') as f:
     lines = f.read().splitlines() 
 
-a = { line.split(',')[0]: line.split(',')[1:]  for line in lines}    
+G = nx.from_dict_of_lists({line.split(',')[0]: line.split(',')[1:] for line in lines})
 
-G = nx.from_dict_of_lists(a)
+len(set(G.nodes))
 
-nx.draw(G)
+G.nodes
 
-len(G)
+# + active=""
+# for city in a.keys():
+#     for c in a[city]:
+#         print(c, city)
+#         assert(city in a[c])
+# -
 
+#source: "https://simplemaps.com/data/tr-cities"
+coordinates_and_population = pd.read_csv("data/coordinates.csv")[["city", "lng", "lat", 'population']]
+
+coordinates_and_population.city = (coordinates_and_population.city
+                                   .str.replace('ş','s')
+                                   .str.replace('ğ','g')
+                                   .str.replace('ı','i')
+                                   .str.replace('ç','c')
+                                   .str.replace('ü','u')
+                                   .str.replace('Ş','S')
+                                   .str.replace('ö','o')
+                                   .str.replace('Ç','C')
+                                   .str.replace('Ü','U')
+                                   .str.replace('İ','I')
+                                   .str.replace('Ö','O')
+                                   .str.replace('Afyonkarahisar','Afyon'))
+
+coordinates_and_population = coordinates_and_population.set_index('city')
+coordinates_and_population = coordinates_and_population.loc[~coordinates_and_population.index.duplicated(keep='first')]
+coordinates_and_population = coordinates_and_population.reindex(G.nodes)
+
+len(coordinates_and_population)
+
+pos = coordinates_and_population[["lng", "lat"]].T.to_dict(orient='list')
+
+# +
+#biggest_30_cities = coordinates_and_population.population.sort_values(ascending=False).head(30).index
+# -
+
+#source: https://www.haberturk.com/giris-cikis-yasaklanan-iller-hangisidir-30-buyuksehir-listesi-2020-2635405
+restricted_cities = ["Adana", "Ankara", "Antalya", "Aydın", "Balıkesir", "Bursa", "Denizli", "Diyarbakır", 
+ "Erzurum", "Eskişehir", "Gaziantep", "Hatay", "İstanbul", "İzmir", "Kahramanmaraş", 
+ "Kayseri", "Kocaeli", "Konya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", 
+ "Ordu", "Sakarya", "Samsun", "Şanlıurfa", "Tekirdağ", "Trabzon", "Van", "Zonguldak"]
+
+restricted_cities = (pd.Series(restricted_cities)
+ .str.replace('ş','s')
+ .str.replace('ğ','g')
+ .str.replace('ı','i')
+ .str.replace('ç','c')
+ .str.replace('ü','u')
+ .str.replace('Ş','S')
+ .str.replace('ö','o')
+ .str.replace('Ç','C')
+ .str.replace('Ü','U')
+ .str.replace('İ','I')
+ .str.replace('Ö','O')
+ .str.replace('Afyonkarahisar','Afyon')).values
+
+plt.figure(figsize=(18,8)) 
+nodes = nx.draw_networkx_nodes(G, pos=pos, alpha=0.3, node_size=100, node_color='green')
+big_cities = nx.draw_networkx_nodes(G, nodelist=restricted_cities, pos=pos, alpha=0.2, node_size=500, node_color='red')
+labels = nx.draw_networkx_labels(G, pos=pos)
+edges = nx.draw_networkx_edges(G, pos=pos, alpha=0.3)
+
+# 2-approx. algorithm for min. vertex cover
 len(min_weighted_vertex_cover(G))
 
+# maximum matching number is a lower bound for the min. vertex cover number
 len(nx.max_weight_matching(G))
 
-nx.planar_layout(G)
+# |V| = a minimal vertex cover + a maximal independent set
+len(G.nodes) - len(nx.algorithms.mis.maximal_independent_set(G))
 
-(_, H) = nx.algorithms.check_planarity(G, counterexample=True)
+vertex_cover = set(G.nodes).difference(nx.algorithms.mis.maximal_independent_set(G))
 
-plt.figure(figsize=(12,8)) 
-pos = nx.spring_layout(H, scale=2)
-nodes = nx.draw_networkx_nodes(H, pos=pos)
-labels = nx.draw_networkx_labels(H, pos=pos)
-edges = nx.draw_networkx_edges(H, pos=pos)
-
-S = [node for node, value in H.degree if H.degree[node] > 2]
-
-H_prime = H.subgraph(S)
-
-plt.figure(figsize=(12,4)) 
-pos = nx.circular_layout(H_prime)
-nodes = nx.draw_networkx_nodes(H_prime, pos=pos)
-labels = nx.draw_networkx_labels(H_prime, pos=pos)
-edges = nx.draw_networkx_edges(H_prime, pos=pos)
+plt.figure(figsize=(18,8)) 
+nodes = nx.draw_networkx_nodes(G, pos=pos, alpha=0.3, node_size=100, node_color='green')
+vc = nx.draw_networkx_nodes(G, nodelist=vertex_cover, pos=pos, alpha=0.2, node_size=500, node_color='blue')
+labels = nx.draw_networkx_labels(G, pos=pos)
+edges = nx.draw_networkx_edges(G, pos=pos, alpha=0.3)
